4c2db7bb7b22366a65b994eff4a9f5923b688561
kyungmin2322 <kyungmin2322@gmail.com>
2025-01-31 12:20:40 +0900

    2025.01.31 12:20

--- 250131/Assets/Scripts/VectorSample.cs
+++ 250131/Assets/Scripts/VectorSample.cs
@@ -5,9 +5,9 @@ public class VectorSample : MonoBehaviour
     // 기본 벡터 (x, y, z) 순으로 값이 작성됨.
     Vector3 vec = new Vector3();
 
-    float x, y, z;
+    float x = 10.0f, y = 20.0f, z = 30.0f;
 
-    Vector3 custom_vec = new Vector3(x, y, z);
+    Vector3 custom_vec = new Vector3();
 
     // 유니티 기본 벡터(제공 값)
     // Vector3 a = Vector3.up;
@@ -17,9 +17,23 @@ public class VectorSample : MonoBehaviour
     // right : (1, 0, 0)
     // forward : (0, 0, 1)
 
+    // 벡터 기본 연산(덧셈, 뺄셈, 나눗셈, 곱셈)
+    Vector3 a = new Vector3(1, 2, 0);
+    Vector3 b = new Vector3(3, 4, 0);
+
+    Vector3 some = Vector3.zero;
+    float point = 5.0f;
+
+    // 벡터 문법
+
     void Start()
     {
-        
+        // 덧셈 : 단계적으로 하나씩 차례대로 처리한다.
+        // 순서가 변경되어도 결과는 같다.
+        // 특정 포지션에서 점프한 느낌의 계산 처리
+        Vector3 c = a + b;
+
+        var trap_air = some + new Vector3(0, point);
     }
 
     void Update()
